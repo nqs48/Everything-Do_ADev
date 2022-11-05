455d59e6ab6108406eeed2ec617ec51b66daab91
Nestor Quiroga <nqs48@icloud.com>
2022-11-05 17:54:34 -0500

    Add new function sweet alert
    the new function is confirmation alert

--- assets/js/sweet.alerts.js
+++ assets/js/sweet.alerts.js
@@ -1,21 +1,62 @@
-
-
-export const success= (title) => {
+export const success = (title) => {
   Swal.fire({
     icon: "success",
     title: title,
     showConfirmButton: false,
     timer: 1500,
+    confirmButtonColor: "rgb(239 68 68)",
+    showClass: {
+      popup: "animate__animated animate__fadeInDown animated__slow",
+    },
+    hideClass: {
+      popup: "animate__animated animate__fadeOutUp animated__slow",
+    },
+    backdrop: `
+    rgba(38, 70, 83, 0.8)
+  `,
   });
-}
+};
 
-export const error = (text) => {
+export const error = (title,text) => {
   Swal.fire({
     icon: "error",
-    title: "Oops...",
+    title: title,
     text: text,
+    confirmButtonColor: "rgb(38, 70, 83)",
+    showClass: {
+      popup: "animate__animated animate__fadeInDown animated__slow",
+    },
+    hideClass: {
+      popup: "animate__animated animate__fadeOutUp animated__slow",
+    },
+    backdrop: `
+    rgba(38, 70, 83, 0.8)
+  `,
   });
 };
 
 
 
+
+
+export const confirmation = () => {
+  return Swal.fire({
+    title: "Estas seguro?",
+    text: "No podrás revertir esta acción!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    cancelButtonText: "Cancelar",
+    confirmButtonText: "Si, Confirmado!",
+    showClass: {
+      popup: "animate__animated animate__fadeInDown animated__slow",
+    },
+    hideClass: {
+      popup: "animate__animated animate__fadeOutUp animated__slow",
+    },
+    backdrop: `
+    rgba(38, 70, 83, 0.8)
+  `,
+  });
+};
